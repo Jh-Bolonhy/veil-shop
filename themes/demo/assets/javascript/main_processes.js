978478c9331bf4d5bf0448ae6bab43b5e160b747
Leonid Muzyka <molcha@bk.ru>
2019-02-15 16:18:21 +0300

    Вижуал в хедере: 1. Элемент с сердечком и количеством выбранных картинок. 2. Линк: "получить консультацию" 3. Много стилей.

--- themes/demo/assets/javascript/main_processes.js
+++ themes/demo/assets/javascript/main_processes.js
@@ -17,7 +17,7 @@ jQuery(document).ready(function($){
 			console.log ('not empty');
 
 			for (let i = 0; i < ls_chopic.length; i++){
-				console.log(ls_chopic[i] + " != " + href);
+				// console.log(ls_chopic[i] + " != " + href);
 				if (ls_chopic[i] == href){
 					ls_chopic.splice(i,1);
 					ifDeleting = true;
@@ -40,6 +40,7 @@ jQuery(document).ready(function($){
 		localStorage.setItem("chosen_pics", JSON.stringify(ls_chopic));
 		console.log (localStorage.getItem("chosen_pics"));
 		toggleHeartStatus(href);
+		changingHeartCounter();
 
 	});
 
@@ -49,18 +50,19 @@ jQuery(document).ready(function($){
 	    success: function(data) {
 		    localStorage.setItem("chosen_pics", JSON.stringify(data));
 		    console.log(localStorage.getItem("chosen_pics"));
-		    this.success(data).done(updateHeartStatuses());
+		    this.success(data).done( updateHeartStatuses() );
 	    }
 		});
 	};
 
 	function updateHeartStatuses() {
-		console.log ('updating hearts');
+		console.log ('updating heart statuses');
 		var ls_chosen_pics_arr = JSON.parse(localStorage.getItem("chosen_pics"));
 
 		ls_chosen_pics_arr.forEach(function (subArrayItem) {
 			toggleHeartStatus(subArrayItem);
 		});
+		changingHeartCounter();
 	};
 
 	function toggleHeartStatus(pic_url) {
@@ -76,8 +78,27 @@ jQuery(document).ready(function($){
 		});
 	};
 
+	// Changing all the appearances of the header elements
+	function changingHeartCounter() {
+
+		console.log ('changingHeartCounter');
+		var arr_length = JSON.parse(localStorage.getItem("chosen_pics")).length;
+
+		if (arr_length>0) {
+			$('.heart-counter').html("&nbsp" + arr_length + "&nbsp");
+			$('.heart-counter').addClass("span-appeared");
+			$('.heart-span').addClass("has-numbers");
+			if (!$('.consult-link').parent().hasClass('active-page')){
+				$('.consult-link').addClass("glow");
+			}
+		} else {
+			$('.heart-counter').removeClass("span-appeared");
+			$('.heart-span').removeClass("has-numbers");
+			$('.consult-link').removeClass("glow");
+		}
+
+	}
 
 	updateChosenPics();
-
 
 });
